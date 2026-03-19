0a0e2eb00c198e11859e985a8b77c8a84ef04d91
gustavo <gustavo.guedes@unesp.br>
2026-03-19 08:24:15 -0300

    Atualizando

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<?php 
+    $cidade = "Bauru";
+    $dia = "1";
+    $mes = "Março";
+    $ano = "2024";
+
+    echo "<b>$cidade</b>, $dia de $mes de $ano";
+    /*Meu programa começa com a tag do  php. 
+    Inicio 4 váriaveis: cidade, dia, mês, ano.
+    Após isso eu printo as variáveis na tela com echo e aspas dupla.
+    Fecho a tag php.*/
+?>
