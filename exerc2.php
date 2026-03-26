a04a3df053711e446e9db5e7549350a1eef6f9bd
Gustavo jr. <gustavo.guedes@unesp.br>
2026-03-26 08:46:17 -0300

    DIa 26/03

---
+++ exerc2.php
@@ -0,0 +1,7 @@
+<?php
+    $string1 = "Gustavo";
+    $string2 = " Guedes";
+
+    $string3 = $string1.$string2;
+    echo "$string3";
+?>
---
+++ exerc3.php
@@ -0,0 +1,8 @@
+<?php
+    $php2 = 1;
+    $php3 =  4;
+
+    $php4 = $php2 <= $php3 ?$php4 = "MenorIgual": $php4 = "Verdadeiro";
+    echo"$php4";
+?>
+?>
